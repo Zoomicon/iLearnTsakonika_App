aea44111c688e9064ef9f7d481df591ddfdf3bfa
birbilis <birbilis@zoomicon.com>
2024-12-31 21:51:29 +0200

    fix to GenerateUniversalAPKfromAAB_Release.bat

--- App/Android64/GenerateUniversalAPKfromAAB_Release.bat
+++ App/Android64/GenerateUniversalAPKfromAAB_Release.bat
@@ -4,10 +4,10 @@
 @set /p PWD_KEYSTORE="Keystore password: "
 @set /p PWD_KEY="Key password: "
 
-del READCOM_App.apks
+del iLearnTsakonika_App.apks
 
-java -jar bundletool.jar build-apks --bundle=Release\READCOM_App\bin\READCOM_App.aab --output=READCOM_App.apks --overwrite --mode=universal --ks=%UserProfile%\OneDrive\Zoomicon\Android\Keystore.keystore --ks-pass=pass:%PWD_KEYSTORE% --ks-key-alias=readcom --key-pass=pass:%PWD_KEY%
+java -jar bundletool.jar build-apks --bundle=Release\iLearnTsakonika_App\bin\iLearnTsakonika_App.aab --output=iLearnTsakonika_App.apks --overwrite --mode=universal --ks=%UserProfile%\OneDrive\Zoomicon\Android\Keystore.keystore --ks-pass=pass:%PWD_KEYSTORE% --ks-key-alias=readcom --key-pass=pass:%PWD_KEY%
 
-echo Open the .apks with a ZIP extractor (or rename to .zip) and get the universal .apk from there (should rename to READCOM_App.universal.apk). Can also call touch on the .apk filename else it has very old/default date
+echo Open the .apks with a ZIP extractor (or rename to .zip) and get the universal .apk from there (should rename to iLearnTsakonika_App.universal.apk). Can also call touch on the .apk filename else it has very old/default date
 
 @pause
