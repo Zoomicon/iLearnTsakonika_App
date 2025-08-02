55964745eba2ac74050da7fe029bb13860c76bae
birbilis <birbilis@zoomicon.com>
2025-08-02 20:05:18 +0300

    - using fullscreen by default on Android (on Windows still has issues, e.g. not showing color popups)

--- App/iLearnTsakonika.App.Events.pas
+++ App/iLearnTsakonika.App.Events.pas
@@ -18,8 +18,14 @@ implementation
 
   procedure TEventHandlers.StoryFormReady(const StoryForm: TStoryForm);
   begin
+    //Untoggle actionMenu / hide layoutButtons by default (assuming content has its own navigation StoryItems with UrlAction set)
     StoryForm.HUD.actionMenu.Checked := false; //don't just do StoryForm.HUD.layoutButtons.Visible := false since that would make the menu toggle button unsync
     StoryForm.HUD.layoutButtons.Visible := false;
+
+    //Use fullscreen by default
+    {$IF Defined(Android)} //FullScreen still has issues on Windows (e.g. color popups don't show), only do on Android
+    StoryForm.HUD.btnToggleFullscreen.IsPressed := true; //set FullScreen //don't use "Pressed" instead
+    {$ENDIF}
   end;
 
   procedure TEventHandlers.StoryLoaded(const Story: TStory);
