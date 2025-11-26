9f7333da0e0d484f0a9de0b8ff6ee3d49c3e3a9e
birbilis <birbilis@zoomicon.com>
2025-11-26 03:34:06 +0200

    - Version info 8.0.0 (Code 17 on Android)

--- App/Android64/GenerateUniversalAPKfromAAB_Release.bat
+++ App/Android64/GenerateUniversalAPKfromAAB_Release.bat
@@ -6,7 +6,7 @@
 
 del iLearnTsakonika_App.apks
 
-java -jar bundletool.jar build-apks --bundle=Release\iLearnTsakonika_App\bin\iLearnTsakonika_App.aab --output=iLearnTsakonika_App.apks --overwrite --mode=universal --ks=%UserProfile%\OneDrive\Zoomicon\Android\Keystore.keystore --ks-pass=pass:%PWD_KEYSTORE% --ks-key-alias=ilearntsakonika --key-pass=pass:%PWD_KEY%
+java -jar bundletool.jar build-apks --bundle=Release\iLearnTsakonika_App\bin\iLearnTsakonika_App.aab --output=iLearnTsakonika_App.apks --overwrite --mode=universal --ks=%UserProfile%\Zoomicon\Android\Keystore.keystore --ks-pass=pass:%PWD_KEYSTORE% --ks-key-alias=ilearntsakonika --key-pass=pass:%PWD_KEY%
 
 echo Open the .apks with a ZIP extractor (or rename to .zip) and get the universal .apk from there (should rename to iLearnTsakonika_App.universal.apk). Can also call touch on the .apk filename else it has very old/default date
 
